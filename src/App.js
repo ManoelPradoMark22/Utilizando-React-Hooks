c022b44ff2b99eb02191fb4e0831b381597af0fd
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-10-22 12:50:55 -0300

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [techs, setTech] = useState(['ReactJS', 'React Native']); // desestruturação
+  /* sempre coloque o estado inicial, nesse caso pode ser um array vazio
+  ou já com algumas tecnologias inicialmente.
+  Para cada tipo de info dentro do componente teremos um estado separado.
+  agora para cada estado temos um useState() separado.
+  primeira pos: retorna o estado em si, nesse caso a nossa lista de tecnologias - techs
+  segunda pos: função para atualizar as infos do estado */
+  const [newTech, setNewTech] = useState(''); // estado inicial
+
+  function handleAdd() {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }
+
+  /* toda vez q alteramos a variavel techs o return executa novamente, como o render */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {techs.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
   );
 }
 
