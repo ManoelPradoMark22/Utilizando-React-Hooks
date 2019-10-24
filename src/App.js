9dc3296532ab0bc620158c81f73184b48491688d
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-10-24 11:12:08 -0300

    Hooks useMemo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
   const [techs, setTech] = useState([]); // desestruturação
@@ -10,10 +10,21 @@ function App() {
   segunda pos: função para atualizar as infos do estado */
   const [newTech, setNewTech] = useState(''); // estado inicial
 
+  /* Dessa forma gasta mto processamento do JS, já que essa fç é re-criada do zero
+  sempre q alterarmos as variaveis utilizadas nela, mesmo quando apenas colocamos
+  uma letra no input.
   function handleAdd() {
     setTech([...techs, newTech]);
     setNewTech('');
   }
+  Então usaremos o useCallback(), assim a fç handleAdd só vai ser recriada na
+  memória qnd a variavel techs ou newTech sofrerem alterações.
+  utilizar o useCallback() apenas em fçs q utilizam os estados, propriedades do
+  componente ou qualquer tipo de variavel do useState */
+  const handleAdd = useCallback(() => {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
 
   // simula o componentDidMount
   useEffect(() => {
@@ -29,6 +40,10 @@ function App() {
     localStorage.setItem('techs', JSON.stringify(techs));
   }, [techs]);
 
+  /* a variavel techsSize so vai executar a funcao techs.length de novo caso
+  a variavel techs esteja com um VALOR diferente! */
+  const techsSize = useMemo(() => techs.length, [techs]);
+
   /* toda vez q alteramos a variavel techs o return executa novamente, como o render */
   return (
     <>
@@ -37,6 +52,16 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+
+      {/* Não é recomendado fazer dessa forma a seguir, pq toda vez q fosse renderizado
+      iria executar novamente (e para calculos mais complexos é ruim). entao
+      usaremos o useMemo() pq executaremos o trecho de codigo techs.lenght
+      APENAS quando a variável techs mudar (e da forma tradicional a seguir
+      executaria sempre q escrevessemos uma letra no input, renderizassemos a
+      pagina etc) */}
+      <strong>Você tem {techsSize} tecnologias</strong>
+      <br />
+
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
